b41199216ded904d7ee629682e3bfc293d175adb
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-06-07 21:06:22 +0300

    Array and Collections

--- Apps/Program.cs
+++ Apps/Program.cs
@@ -1,4 +1,23 @@
-﻿var arrChar = new char[3] {'a','v','d'};
-var arrInt= Array.CreateInstance(typeof(int), 5);
-arrInt.SetValue(10, 0);
-arrInt.GetValue(0);
+﻿using System.Collections;
+
+//Array
+var arrChar = new char[3] {'a','v','d'};
+Console.WriteLine(arrChar.Length);
+
+//ArrayList
+var arrObj = new ArrayList();
+arrObj.Add(10);
+arrObj.Add("b");
+Console.WriteLine(arrObj.Count);
+
+//List<T>
+var arrInt = new List<int>();
+arrInt.Add(10);
+arrInt.Add(15);
+arrInt.RemoveAt(0);
+Console.WriteLine(arrInt.Count);
+foreach (var item in arrInt)
+{
+    Console.WriteLine(item);
+}
+Console.ReadKey();
