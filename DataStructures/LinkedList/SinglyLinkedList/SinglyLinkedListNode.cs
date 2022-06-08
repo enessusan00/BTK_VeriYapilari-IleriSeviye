bcdd6c283369595247ea7eca5da540998c91db39
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-06-08 16:09:42 +0300

    Singly Linked List

---
+++ DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedListNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.SinglyLinkedList
+{
+    public class SinglyLinkedListNode<T>
+    {
+        public SinglyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public SinglyLinkedListNode<T> Next { get; set; }
+        public T Value { get; set; }
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+
+    }
+}
