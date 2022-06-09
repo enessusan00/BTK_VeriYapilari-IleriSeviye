29729360d34c81d93e39bf96701a150b76b3778d
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-06-09 13:36:29 +0300

    initial

--- Apps/Program.cs
+++ Apps/Program.cs
@@ -58,7 +58,7 @@ linkedList.AddFirst(10);
 linkedList.AddFirst(15);    
 linkedList.AddFirst(30);
 linkedList.AddFirst(5);
-linkedList.AddLast(20);
+linkedList.AddLast(31);
 linkedList.AddAfter(linkedList.Head.Next, 50);
 
 #endregion
