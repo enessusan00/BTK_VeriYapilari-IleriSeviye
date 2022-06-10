52b447b2c3ef98af6279b04856f566f466fd0812
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-06-10 14:04:36 +0300

    Array Queue

--- DataStructures/Queue/ArrayQueue.cs
+++ DataStructures/Queue/ArrayQueue.cs
@@ -2,21 +2,32 @@
 {
     internal class ArrayQueue<T> : IQueue<T>
     {
-        public int Count => throw new NotImplementedException();
+        private readonly List<T> list = new List<T> ();
+        public int Count { get; private set; }
 
         public T DeQueue()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+                throw new Exception("Empty queue");
+            var temp = list[0];
+            list.RemoveAt (0);
+            Count--;
+            return temp;
         }
 
         public void EnQueue(T value)
         {
-            throw new NotImplementedException();
+            if(value == null)
+                throw new ArgumentNullException();
+            list.Add (value);
+            Count++;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if(Count == 0)
+                throw new Exception("Empty queue");
+            return list[0];
         }
     }
 }
