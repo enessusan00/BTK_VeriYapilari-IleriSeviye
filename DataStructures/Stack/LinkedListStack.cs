8c217fc11b94089e03da9c6356f8e1eda0c9b69c
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-06-10 08:24:58 +0300

    Stack<T> implementation

---
+++ DataStructures/Stack/LinkedListStack.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructures.Stack
+{
+    internal class LinkedListStack<T> : IStack<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
