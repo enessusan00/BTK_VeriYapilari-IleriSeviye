e71218eaa5b62fc40f820fcaab28e113a9164438
enessusan00@hotmail.com <enessusan00@hotmail.com>
2022-06-12 19:19:35 +0300

    HeapifyUp / Down

--- DataStructures/Heap/BHeap.cs
+++ DataStructures/Heap/BHeap.cs
@@ -11,8 +11,8 @@ namespace DataStructures.Heap
         where T : IComparable<T>
     {
         public T[] Array { get; private set; }
-        private int position;
-        public int Count { get; set; }
+        protected int position;
+        public int Count { get; private set; }
         public BHeap()
         {
             Count = 0;
@@ -35,44 +35,44 @@ namespace DataStructures.Heap
                 Add(item);
             }
         }
-        private int GetLeftChildIndex(int i) => 2 * i + 1;
-        private int GetRightChildIndex(int i) => 2 * i + 2;
-        private int GetParentIndex(int i) => i-1/2;
-        private bool HasLeftChild(int i) => 
+
+        protected int GetLeftChildIndex(int i) => 2 * i + 1;
+        protected int GetRightChildIndex(int i) => 2 * i + 2;
+        protected int GetParentIndex(int i) => (i - 1) / 2;
+        protected bool HasLeftChild(int i) =>
             GetLeftChildIndex(i) < position;
-        private bool HasRightChild(int i) => 
+        protected bool HasRightChild(int i) =>
             GetRightChildIndex(i) < position;
-        private bool IsRoot(int i) => i == 0;
-        private T GetLeftChild(int i) => Array[GetLeftChildIndex(i)];
-        private T GetRightChild(int i) => Array[GetRightChildIndex(i)];
-        private T GetParent(int i ) => Array[GetParentIndex(i)];    
-        public bool IsEmpty()=> position == 0;
+        protected bool IsRoot(int i) => i == 0;
+        protected T GetLeftChild(int i) => Array[GetLeftChildIndex(i)];
+        protected T GetRightChild(int i) => Array[GetRightChildIndex(i)];
+        protected T GetParent(int i) => Array[GetParentIndex(i)];
+        public bool IsEmpty() => position == 0;
         public T Peek()
         {
-            if(IsEmpty())
-            {
-                throw new Exception("Empty heap");
-            }
+            if (IsEmpty())
+                throw new Exception("Empty heap!");
             return Array[0];
         }
-        public void Swap(int first,int second)
+
+        public void Swap(int first, int second)
         {
-            var temp=Array[first];
-            Array[first]=Array[second];
-            Array[second]=temp;
+            var temp = Array[first];
+            Array[first] = Array[second];
+            Array[second] = temp;
         }
         public void Add(T value)
         {
             if (position == Array.Length)
                 throw new IndexOutOfRangeException("Overflow!");
-            Array[position]=value;
+            Array[position] = value;
             position++;
             Count++;
             HeapifyUp();
         }
         public T DeleteMinMax()
         {
-            if (position == Array.Length)
+            if (position == 0)
                 throw new IndexOutOfRangeException("Underflow!");
             var temp = Array[0];
             Array[0] = Array[position - 1];
@@ -80,10 +80,11 @@ namespace DataStructures.Heap
             Count--;
             HeapifyDown();
             return temp;
-
         }
+
         protected abstract void HeapifyUp();
         protected abstract void HeapifyDown();
+
         public IEnumerator<T> GetEnumerator()
         {
             return Array.Take(position).GetEnumerator();
@@ -92,19 +93,6 @@ namespace DataStructures.Heap
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
-        }
-    }
-    public class MinHeap<T> : BHeap<T>, IEnumerable<T>
-        where T : IComparable<T>
-    {
-        protected override void HeapifyDown()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override void HeapifyUp()
-        {
-            throw new NotImplementedException();
         }
     }
 }
